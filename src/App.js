fe50ed761401869955dca2445f43b515d1a882c6
dhilip <dhiliptry7@gmail.com>
2024-06-24 17:32:27 +0530

    mongoDb

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Feed from "./pages/feed/Feed";
+import Create from "./pages/create/Create";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/employer">
+          <Route path="/employer/dashboard" element={<Dashboard />} />
+          <Route path="/employer/create" element={<Create />} />
+        </Route>
+        <Route path="/employee/feed" element={<Feed />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/index/Index.js
@@ -0,0 +1,4 @@
+export { default as Dashboard } from "../dashboard/Dashboard";
+export { default as Home } from "../home/Home";
+export { default as Feed } from "../feed/Feed";
+export { default as Create } from "../create/Create";
